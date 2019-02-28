cfa702ada762640e5b49a47f368f6561ed1137d3
Stephen <stephen@thoughtstem.com>
2019-02-28 10:53:00 -0800

    first

--- info.rkt
+++ info.rkt
@@ -1,13 +1,18 @@
 #lang info
 (define collection "ts-all-dev")
 (define deps '("base"
-               "https://github.com/thoughtstem/racket-chipmunk.git#dev"  
-               "https://github.com/thoughtstem/game-engine.git#dev" 
-               "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#dev" 
-               "https://github.com/thoughtstem/game-engine-rpg.git#dev" 
-               "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util#dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena#dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers#dev"
+               "https://github.com/thoughtstem/racket-chipmunk.git"  
+               "https://github.com/thoughtstem/game-engine.git" 
+               "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common" 
+               "https://github.com/thoughtstem/game-engine-rpg.git" 
+               "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util" 
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena" 
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers"
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-starwars"
+               "https://github.com/thoughtstem/TS-Languages.git?path=survival"
+               "https://github.com/thoughtstem/TS-Languages.git?path=survival-minecraft"
+               "https://github.com/thoughtstem/TS-Languages.git?path=fundamentals"
+               "https://github.com/thoughtstem/TS-Languages.git?path=k2"
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ts-all-dev.scrbl" ())))
