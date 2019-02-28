9612c0c948936dad825a9edc008269f59fe255ca
Stephen <stephen@thoughtstem.com>
2019-02-28 15:43:18 -0800

    breaking on purpose to prove that travis is not getting the most recent versoin

--- info.rkt
+++ info.rkt
@@ -1,20 +1,7 @@
 #lang info
 (define collection "ts-all-dev")
-breaking this on purpose
-(define deps '("base"
-               "https://github.com/thoughtstem/racket-chipmunk.git#dev"  
-               "https://github.com/thoughtstem/ratchet.git#dev"  
 
-               "https://github.com/thoughtstem/game-engine.git#dev" 
-               "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#dev" 
-               "https://github.com/thoughtstem/game-engine-rpg.git#dev" 
-               "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util#dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena#dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers#dev"
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-starwars#dev"
-               "https://github.com/thoughtstem/TS-Languages.git?path=survival#dev"
-               "https://github.com/thoughtstem/TS-Languages.git?path=survival-minecraft#dev"
-               "https://github.com/thoughtstem/TS-Languages.git?path=fundamentals#dev"
+(define deps '("base"
                "https://github.com/thoughtstem/TS-Languages.git?path=k2#dev"
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
