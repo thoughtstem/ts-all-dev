a1b28b4e2ea0968e90b1c1669417c13ae27b636f
Stephen <stephen@thoughtstem.com>
2019-02-28 08:37:12 -0800

    fixing how to specify dev branch

--- info.rkt
+++ info.rkt
@@ -1,14 +1,13 @@
 #lang info
 (define collection "ts-all-dev")
 (define deps '("base"
-               "https://github.com/thoughtstem/racket-chipmunk.git?branch=dev" 
-               "https://github.com/thoughtstem/game-engine.git?branch=dev" 
-               "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common&branch=dev" 
-               "https://github.com/thoughtstem/game-engine-rpg.git?branch=dev" 
-               "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util&branch=dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena&branch=dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers&branch=dev"
-               
+               "https://github.com/thoughtstem/racket-chipmunk.git#dev" 
+               "https://github.com/thoughtstem/game-engine.git#dev" 
+               "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#dev" 
+               "https://github.com/thoughtstem/game-engine-rpg.git#dev" 
+               "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util#dev" 
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena#dev" 
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers#dev"
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ts-all-dev.scrbl" ())))
