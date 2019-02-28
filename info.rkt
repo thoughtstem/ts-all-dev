e207f40878019c06ebef7f8d9aeec4102d3e8977
Stephen <stephen@thoughtstem.com>
2019-02-28 10:57:35 -0800

    fixing #devs

--- info.rkt
+++ info.rkt
@@ -8,6 +8,11 @@
                "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util#dev" 
                "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena#dev" 
                "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers#dev"
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-starwars#dev"
+               "https://github.com/thoughtstem/TS-Languages.git?path=survival#dev"
+               "https://github.com/thoughtstem/TS-Languages.git?path=survival-minecraft#dev"
+               "https://github.com/thoughtstem/TS-Languages.git?path=fundamentals#dev"
+               "https://github.com/thoughtstem/TS-Languages.git?path=k2#dev"
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ts-all-dev.scrbl" ())))
