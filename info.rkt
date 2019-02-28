bc4e5ede1050426570eeeaa1bc8ebac15d6015a4
Stephen <stephen@thoughtstem.com>
2019-02-27 16:05:28 -0800

    adding deps

--- info.rkt
+++ info.rkt
@@ -1,10 +1,15 @@
 #lang info
 (define collection "ts-all-dev")
 (define deps '("base"
+               "https://github.com/thoughtstem/racket-chipmunk.git?branch=dev" 
                "https://github.com/thoughtstem/game-engine.git?branch=dev" 
+               "https://github.com/thoughtstem/game-engine-demos-common.git?branch=dev" 
+               "https://github.com/thoughtstem/game-engine-rpg.git?branch=dev" 
                "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util&branch=dev" 
                "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena&branch=dev" 
-               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers&branch=dev"))
+               "https://github.com/thoughtstem/TS-Languages.git?path=battle-arena-avengers&branch=dev"
+               
+               ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ts-all-dev.scrbl" ())))
 (define pkg-desc "Description Here")
